446a39a5102a33294e0e4984660d787db7a6e0ae
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 12:05:41 -0400

    Update streamlit3_app.py

--- streamlit3_app.py
+++ streamlit3_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 from PIL import Image
 
 image = Image.open('Shared bicycle rental process.png')
-
+st.title("Visualizing concepts and qualitative data")  # add a title
 st.image(image, caption='Shared bicycle rental process')
