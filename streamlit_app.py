be3f8a8a2bff7b3f1bf117d7bdc336eed2e9e3c6
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 12:59:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,16 @@ df = pd.read_csv('london_merged.csv', parse_dates=True, index_col='timestamp')
 st.title("Visualizing correlation, comparisons, and trends")  # add a title
 st.write(df)  # visualize my dataframe in the Streamlit app
 
-with st.echo(code_location='below'):
+#with st.echo(code_location='below'):
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
+   # fig = plt.figure()
+   # ax = fig.add_subplot(1,1,1)
 
-    sns.pointplot(ax =ax,data=df, x=df.index.hour, y='cnt', hue='is_holiday')
-    ax.set_title("Bicycle share usage volumn in holiday and non-holiday")
-    ax.set_xlabel("Day Hour")
-    ax.set_ylabel("Bicycle share usage volumn")
-    st.write(fig)
+   # sns.pointplot(ax =ax,data=df, x=df.index.hour, y='cnt', hue='is_holiday')
+   # ax.set_title("Bicycle share usage volumn in holiday and non-holiday")
+   # ax.set_xlabel("Day Hour")
+  #  ax.set_ylabel("Bicycle share usage volumn")
+   # st.write(fig)
+    
+
+st.bar_chart(df[['in_holiday','is_weekend','season']])
