ef394f450a501c936c12b5ba6c24e64b2bfeb67b
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 10:56:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,19 @@ st.write(df)  # visualize my dataframe in the Streamlit app
 
 
 #plt.figure(figsize=(10, 4))
-fig, ax = plt.subplots()
-sns.pointplot(ax = ax, data=df, x=df.index.hour, y='cnt', hue='is_holiday')#.set(title='Bicycle share usage volumn in holiday and non-holiday')
-st.pyplot(fig)
+#fig, ax = plt.subplots()
+#sns.pointplot(ax = ax, data=df, x=df.index.hour, y='cnt', hue='is_holiday')#.set(title='Bicycle share usage volumn in holiday and non-holiday')
+#st.pyplot(fig)
+
+with st.echo(code_location='below'):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    ax.scatter(
+        df.index.hour,
+        df['cnt'],
+    )
+
+    ax.set_xlabel("Acceleration")
+    ax.set_ylabel("Miles per gallon")
