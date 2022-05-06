c9ac89834ee0106055c4346c18ca5a4a6030a384
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 13:59:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
 
 df = pd.read_csv('london_merged.csv', parse_dates=True, index_col='timestamp')
 
@@ -19,8 +20,50 @@ st.write(df)  # visualize my dataframe in the Streamlit app
    # ax.set_xlabel("Day Hour")
   #  ax.set_ylabel("Bicycle share usage volumn")
    # st.write(fig)
-    
 
-st.bar_chart(df[['is_holiday']])
-st.bar_chart(df[['is_weekend']])
-st.bar_chart(df[['season']])
+
+  
+st.title("Stroke Prediction Dashboard")
+st.markdown("The dashboard will help a researcher to get to know \
+more about the given datasets and it's output")
+st.sidebar.title("Select Visual Charts")
+st.sidebar.markdown("Select the Charts/Plots accordingly:")
+
+  
+chart_visual = st.sidebar.selectbox('Select Charts/Plot type', 
+                                    ('Line Chart', 'Bar Chart'))
+  
+st.sidebar.checkbox("Show Analysis by Smoking Status", True, key = 1)
+selected_status = st.sidebar.selectbox('Select Smoking Status',
+                                       options = ['is_holiday', 
+                                                  'is_weekend', 'season'])
+  
+fig = go.Figure()
+  
+if chart_visual == 'Line Chart':
+    if selected_status == 'is_holiday':
+        fig.add_trace(go.Scatter(x = df.cnt, y = df[['is_holiday']],
+                                 mode = 'lines',
+                                 name = 'is_holiday'))
+    if selected_status == 'is_weekend':
+        fig.add_trace(go.Scatter(x = df.cnt, y = df[['is_weekend']],
+                                 mode = 'lines', name = 'is_weekend'))
+    if selected_status == 'season':
+        fig.add_trace(go.Scatter(x = df.cnt, y = df[['season']],
+                                 mode = 'lines',
+                                 name = 'season'))
+
+elif chart_visual == 'Bar Chart':
+    if selected_status == 'is_holiday':
+        fig.add_trace(go.Scatter(x = df.cnt, y = df[['is_holiday']],
+                                 mode = 'lines',
+                                 name = 'is_holiday'))
+    if selected_status == 'is_weekend':
+        fig.add_trace(go.Scatter(x = df.cnt, y = df[['is_weekend']],
+                                 mode = 'lines', name = 'is_weekend'))
+    if selected_status == 'season':
+        fig.add_trace(go.Scatter(x = df.cnt, y = df[['season']],
+                                 mode = 'lines',
+                                 name = 'season'))
+  
+st.plotly_chart(fig, use_container_width=True)
