f03a8be94a63ca06db7e5f671279a3f26fcaa0ee
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 12:36:03 -0400

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -10,13 +10,15 @@ df_1D=df.resample('1D').sum()
 st.title("Visualizing distributions and part-to-whole")  # add a title
 st.write(df)  # visualize my dataframe in the Streamlit app
 
-with st.echo(code_location='below'):
+#with st.echo(code_location='below'):
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
+  #  fig = plt.figure()
+   # ax = fig.add_subplot(1,1,1)
 
-    sns.lineplot(x=df_1D.index, y='cnt', data=df_1D)
-    ax.set_title("The distribution of bicycle usage over time")
-    ax.set_xlabel("timestamp")
-    ax.set_ylabel("Bicycle share usage volumn")
-    st.write(fig)
+  #  sns.lineplot(x=df_1D.index, y='cnt', data=df_1D)
+  #  ax.set_title("The distribution of bicycle usage over time")
+   # ax.set_xlabel("timestamp")
+  #  ax.set_ylabel("Bicycle share usage volumn")
+  #  st.write(fig)
+    
+    st.line_chart(df_1D.cnt)
