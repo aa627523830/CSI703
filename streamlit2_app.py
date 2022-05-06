6753907ca50c84fcf75aa07482dbc19afc883172
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 11:39:42 -0400

    Update streamlit2_app.py

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -3,5 +3,5 @@ import pandas as pd
 import numpy as np
 
 st.title("Visualizing geospatial data")  # add a title
-df = pd.read_csv('london_merged.csv', parse_dates=True, index_col='timestamp')
+df = pd.read_csv('dataset_200k.csv')
 st.map(df)
