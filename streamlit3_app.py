bac0eb7cbd11a23cde85b2640706175a069a3187
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 14:27:41 -0400

    Update streamlit3_app.py

--- streamlit3_app.py
+++ streamlit3_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
+from streamlit_player import st_player
 
 image = Image.open('Shared bicycle rental process.png')
 st.title("Visualizing concepts and qualitative data")  # add a title
 st.image(image, caption='Shared bicycle rental process')
+
+# Embed a youtube video
+st_player("https://www.youtube.com/watch?v=xjmUhpO8vU0")
