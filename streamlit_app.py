b2a46e944b5fa5c15125ffc158f13e6d0e6a92b6
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 10:38:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ df = pd.read_csv('london_merged.csv', parse_dates=True, index_col='timestamp')
 st.title("Hello world!")  # add a title
 st.write(df)  # visualize my dataframe in the Streamlit app
 
-def pointplot():
-    fig = plt.figure(figsize=(10, 4))
-    sns.pointplot(data=df, x=df.index.hour, y='cnt', hue='is_holiday').set(title='Bicycle share usage volumn in holiday and non-holiday')
-    st.pyplot(fig)
+
+fig = plt.figure(figsize=(10, 4))
+sns.pointplot(data=df, x=df.index.hour, y='cnt', hue='is_holiday').set(title='Bicycle share usage volumn in holiday and non-holiday')
+st.pyplot(fig)
