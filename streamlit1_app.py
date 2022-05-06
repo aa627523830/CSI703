9fe9d1c01e014f5db42e0a95a11519875388f03f
aa627523830 <53019362+aa627523830@users.noreply.github.com>
2022-05-06 11:21:50 -0400

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('london_merged.csv', parse_dates=True, index_col='timestamp')
+df_1D=df.resample('1D').sum()
 
-st.title("Visualizing correlation, comparisons, and trends")  # add a title
+st.title("Visualizing distributions and part-to-whole")  # add a title
 st.write(df)  # visualize my dataframe in the Streamlit app
 
 with st.echo(code_location='below'):
@@ -14,32 +15,8 @@ with st.echo(code_location='below'):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
 
-    sns.pointplot(ax =ax,data=df, x=df.index.hour, y='cnt', hue='is_holiday')
-    ax.set_title("Bicycle share usage volumn in holiday and non-holiday")
-    ax.set_xlabel("Day Hour")
-    ax.set_ylabel("Bicycle share usage volumn")
-    st.write(fig)
-
-    
-with st.echo(code_location='below'):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-
-    sns.pointplot(data=df, x=df.index.hour, y='cnt', hue='is_weekend')
-    ax.set_title("Bicycle share usage volumn in weekend and non-weekend")
-    ax.set_xlabel("Day Hour")
-    ax.set_ylabel("Bicycle share usage volumn")
-    st.write(fig)
-
-    
-with st.echo(code_location='below'):
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-
-    sns.pointplot(data=df, x=df.index.hour, y='cnt', hue='season')
-    ax.set_title("Bicycle share usage volumn in different season")
-    ax.set_xlabel("Day Hour")
+    sns.lineplot(x=df_1D.index, y='cnt', data=df_1D)
+    ax.set_title("The distribution of bicycle usage over time")
+    ax.set_xlabel("timestamp")
     ax.set_ylabel("Bicycle share usage volumn")
     st.write(fig)
